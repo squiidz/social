6eb94fa7927520881a78fde7a2cea1f421ba5588
jchaput <piinkyz05@gmail.com>
2018-07-01 23:19:23 -0400

    removed gzip and url validator in main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/squiidz/social/graphServer"
 
@@ -13,19 +12,11 @@ import (
 
 func main() {
 	mux := bone.New()
-	mdlwr := claw.New(mw.Logger, mw.Zipper)
-	mux.RegisterValidatorFunc("isNum", isNum)
+	mdlwr := claw.New(mw.Logger)
 	graphServer := graphServer.New()
 
-	mux.GetFunc("/distance/:userFrom|isNum/:userTo|isNum", graphServer.GetRelationHandler)
-	mux.GetFunc("/friends/:userFrom|isNum/:userTo|isNum", graphServer.GetCommonFriendsHandler)
+	mux.GetFunc("/distance/:userFrom/:userTo", graphServer.GetRelationHandler)
+	mux.GetFunc("/friends/:userFrom/:userTo", graphServer.GetCommonFriendsHandler)
 
 	http.ListenAndServe(":8080", mdlwr.Merge(mux))
-}
-
-func isNum(s string) bool {
-	if _, err := strconv.Atoi(s); err == nil {
-		return true
-	}
-	return false
 }
